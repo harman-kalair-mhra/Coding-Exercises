9474cc0e7886b660be87e0a063a065da8a2ee9e6
harmankalair123 <harman.kalair@ada.ac.uk>
2021-02-05 10:29:14 +0000

    added grand total loop

--- shuffle.js
+++ shuffle.js
@@ -1,5 +1,5 @@
 let arrList = [1, 2, 3,  4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]; // list of array 1- 20
-
+let sum = 0
 
 const shuffleArray = array => {
     for (let i = array.length -1; i > 0; i--) {
@@ -18,5 +18,13 @@ console.log("Original List: ", arrList)
 
 let newArray = shuffleArray(arrList);
 
-console.log("Sorted List: ", newArray)
+console.log("Shuffled List: ", newArray)
 
+// grand total
+
+
+for (let i=0; i < arrList.length; i++) {
+    sum = sum + arrList[i];
+}
+
+console.log("Grand Total of this array is: ", sum)

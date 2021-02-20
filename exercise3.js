7c016c4d8303f4977f0c845c4697307bd99c77be
harmankalair123 <harman.kalair@ada.ac.uk>
2021-02-20 14:56:09 +0000

    add example of using filter instead of if statement

--- exercise3.js
+++ exercise3.js
@@ -13,17 +13,38 @@ const afternoon = [
 
 const tasks = [morning, afternoon]
 
-let cost = 0
+let cost = 0;
+const hourly = 25
 
-const dailyCost = tasks.reduce((accumulator, current) => accumulator.concat(current), [])
 
-dailyCost.map(task => {
-    task.duration = task.duration / 60
-    if (task.duration >= 2 ) {
-        cost = cost+ task.duration * 25 // add task duration to the cost each time.
-    }
-    
-})
-console.log(cost)
+const dailyCost = tasks.reduce((accumulator, current) => accumulator.concat(current), []).filter((task) =>{
+     task.duration = task.duration / 60
+     return task.duration >= 2}).map(task => cost = cost+ task.duration  * hourly) // add task duration to the cost each time.
+
+
+
 // console.log(dailyCost)
+console.log(cost)
+
+
+
+// *********************** Solution using if statement *************************/////////////
+
+
+// let cost = 0
+
+
+// const dailyCost = tasks.reduce((accumulator, current) => accumulator.concat(current), [])
+
+
+// dailyCost.map(task => {
+//     task.duration = task.duration / 60
+//     if (task.duration >= 2 ) {
+//         cost = cost+ task.duration * 25 // add task duration to the cost each time.
+//     }
+    
+// })
+// console.log(cost)
+// console.log(dailyCost)
+
 
